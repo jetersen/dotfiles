2ed3d788d796d782541797344688f5cf1c708d85
Joseph Petersen <josephp90@gmail.com>
2021-01-28 20:53:45 +0100

    Add DotEnv

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -252,6 +252,62 @@ function Update-KubeConfig {
   wsl cp /mnt/c/Users/$ENV:USERNAME/.kube/config ~/.kube/config
 }
 
+function DotEnv {
+  [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
+  param(
+    [string] $localEnvFile = ".\.env"
+  )
+
+  #return if no env file
+  if (!( Test-Path $localEnvFile)) {
+    Write-Verbose "No .env file"
+    return
+  }
+
+  #read the local env file
+  $content = Get-Content $localEnvFile -ErrorAction Stop
+  Write-Verbose "Parsed .env file"
+
+  #load the content to environment
+  foreach ($line in $content) {
+
+    if ([string]::IsNullOrWhiteSpace($line)) {
+      Write-Verbose "Skipping empty line"
+      continue
+    }
+
+    #ignore comments
+    if ($line.StartsWith("#")) {
+      Write-Verbose "Skipping comment: $line"
+      continue
+    }
+
+    #get the operator
+    if ($line -like "*:=*") {
+      Write-Verbose "Prefix"
+      $kvp = $line -split ":=", 2
+      $key = $kvp[0].Trim()
+      $value = "{0};{1}" -f $kvp[1].Trim(), [System.Environment]::GetEnvironmentVariable($key)
+    } elseif ($line -like "*=:*") {
+      Write-Verbose "Suffix"
+      $kvp = $line -split "=:", 2
+      $key = $kvp[0].Trim()
+      $value = "{1};{0}" -f $kvp[1].Trim(), [System.Environment]::GetEnvironmentVariable($key)
+    } else {
+      Write-Verbose "Assign"
+      $kvp = $line -split "=", 2
+      $key = $kvp[0].Trim()
+      $value = $kvp[1].Trim()
+    }
+
+    Write-Verbose "$key=$value"
+
+    if ($PSCmdlet.ShouldProcess("environment variable $key", "set value $value")) {
+      [Environment]::SetEnvironmentVariable($key, $value, "Process") | Out-Null
+    }
+  }
+}
+
 # setup cd extras
 $cde.CD_PATH = @($developmentWorkspace)
 
