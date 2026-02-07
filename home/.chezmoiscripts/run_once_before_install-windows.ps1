db6ad2dfa675f8413235287c6e620bc6b3dfa1cb
Joseph Petersen <me@jetersen.dev>
2026-02-07 18:32:50 +0100

    add initial windows setup script

---
+++ home/.chezmoiscripts/run_once_before_install-windows.ps1
@@ -0,0 +1,28 @@
+Write-Host "🥳 First install, let's get you setup!" -ForegroundColor Magenta
+
+# Update winget sources
+Write-Host "📜 Updating winget sources..."
+winget source update --disable-interactivity --ignore-warnings
+
+if (-not (Get-Command "git" -ErrorAction SilentlyContinue)) {
+  Write-Host "📦 Installing Git..."
+  winget install --id Git.Git `
+    --custom '/components="gitlfs," /o:EditorOption=VisualStudioCode /o:CURLOption=WinSSL /o:UseCredentialManager=Enabled' `
+    --silent --source winget --accept-package-agreements --accept-source-agreements
+  Write-Host "✅ Git installed!"
+}
+
+if (-not (Get-Command "gsudo" -ErrorAction SilentlyContinue)) {
+  Write-Host "📦 Installing gsudo..."
+  winget install --id geardog.gsudo `
+    --silent --source winget --accept-package-agreements --accept-source-agreements
+  Write-Host "✅ gsudo installed!"
+}
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+
+gsudo {
+  Write-Host "🌟 Admin super power unlocked..." -ForegroundColor DarkMagenta
+  Set-ExecutionPolicy RemoteSigned -Scope LocalMachine -Force
+  Write-Host "✅ Execution policy set to RemoteSigned for LocalMachine scope!" -ForegroundColor Green
+}
